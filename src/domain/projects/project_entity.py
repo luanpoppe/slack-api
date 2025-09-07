7c1322e527d9f3a140457ea81d5af7a95661a3ab
Luan Rodrigo Poppe Sampaio <luanpoppe@gmail.com>
2025-09-07 20:44:46 -0300

    refactor: migrations and its versions

--- src/domain/projects/project_entity.py
+++ src/domain/projects/project_entity.py
@@ -1,9 +1,20 @@
+from dataclasses import dataclass
 from sqlalchemy import Column, Integer, String
 
 from src.lib.sql_alchemy.engine import Base
 
 
-class Projects(Base):
+@dataclass
+class CreateProjectEntity:
+    name: str
+    description: str
+    question_one: str
+    question_two: str
+    question_three: str
+    id: str | None = None
+
+
+class ProjectEntity(Base):
     __tablename__ = "projects"
 
     id = Column(
@@ -11,5 +22,8 @@ class Projects(Base):
         primary_key=True,
         index=True,
     )
-    name = Column(String(50), nullable=False)
-    email = Column(String(100), unique=True, index=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    description = Column(String(500), nullable=True)
+    question_one = Column(String(500), nullable=True)
+    question_two = Column(String(500), nullable=True)
+    question_three = Column(String(500), nullable=True)
